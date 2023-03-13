4cd21205775e92d35fd622d931ef0dbce5b24a6b
Lada Rozhina <lada.2020@yandex.ru>
2023-03-13 14:29:33 +0100

    Intersection algo and tests changed

--- src/main/java/Intersection.java
+++ src/main/java/Intersection.java
@@ -1,3 +1,5 @@
+import project_utils.Utils;
+
 import java.util.Arrays;
 
 public class Intersection {
@@ -6,28 +8,52 @@ public class Intersection {
      */
 
     public int[] intersection(int[] arr1, int[] arr2){
-        int index = 0;
-        int length = 0;
-        // ищем длину массива
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr2.length; j++) {
-                if (arr1[i] == arr2[j]) {
-                    length++;
-                }
-            }
-        }
-        int[] arrResult = new int[length];
+        if(Utils.checkNullArrayInt(arr1) && Utils.checkEmptyArrayInt(arr1)
+        && Utils.checkNullArrayInt(arr2) && Utils.checkEmptyArrayInt(arr2)) {
+            int index = 0;
+            int length = 0;
 
-        // заполняем массив
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr2.length; j++) {
-                if (arr1[i] == arr2[j]) {
-                    arrResult[index] = arr1[i];
-                    index++;
+            // убираем повторяющиеся элементы в каждом массиве
+            for (int i = 0; i < arr1.length; i++) {
+                for (int j = i + 1; j < arr1.length; j++) {
+                    if (arr1[i] == arr1[j]) {
+                        arr1[j] = Integer.MIN_VALUE;
+                    }
                 }
             }
+
+            for (int i = 0; i < arr2.length; i++) {
+                for (int j = i + 1; j < arr2.length; j++) {
+                    if (arr2[i] == arr2[j]) {
+                        arr2[j] = Integer.MIN_VALUE;
+                    }
+                }
+            }
+
+            // ищем длину массива
+            for (int i = 0; i < arr1.length; i++) {
+                for (int j = 0; j < arr2.length; j++) {
+                    if (arr1[i] == arr2[j]) {
+                        length++;
+                    }
+                }
+            }
+            int[] arrResult = new int[length];
+
+            // заполняем массив
+            for (int i = 0; i < arr1.length; i++) {
+                for (int j = 0; j < arr2.length; j++) {
+                    if (arr1[i] == arr2[j]) {
+                        arrResult[index] = arr1[i];
+                        index++;
+                    }
+                }
+            }
+            return arrResult;
+
         }
-        return arrResult;
+
+        return new int[0];
 
     }
 }
--- src/test/java/IntersectonTest.java
+++ src/test/java/IntersectonTest.java
@@ -20,7 +20,7 @@ public class IntersectonTest {
     @Test
     public void testIntersection_NegativeNumbers_HappyPath() {
         // arrange
-        int[] arr1 = {1, 2, 4, 5, 8, 9};
+        int[] arr1 = {1, 2, 4, 5, 8, 9, 2};
         int[] arr2 = {8, 9, -4, -2};
         int[] expectedResult = {8, 9};
 
@@ -36,6 +36,76 @@ public class IntersectonTest {
         // arrange
         int[] arr1 = {1, 2, 4, 5, 89};
         int[] arr2 = {8, 9, 45};
+        int[] expectedResult = {};
+
+        // act
+        int[] actualResult = new Intersection().intersection(arr1, arr2);
+
+        // assert
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testIntersection_MoreThanOneIntersection_HappyPath() {
+        // arrange
+        int[] arr1 = {5, 1, 2, 4, 5, 89, 5, 4};
+        int[] arr2 = {8, 9, 4, 5};
+        int[] expectedResult = {5, 4};
+
+        // act
+        int[] actualResult = new Intersection().intersection(arr1, arr2);
+
+        // assert
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testIntersection_FirstEmptyArray_Negative() {
+        // arrange
+        int[] arr1 = {};
+        int[] arr2 = {8, 9, 4, 5};
+        int[] expectedResult = {};
+
+        // act
+        int[] actualResult = new Intersection().intersection(arr1, arr2);
+
+        // assert
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testIntersection_FirstNullArray_Negative() {
+        // arrange
+        int[] arr1 = null;
+        int[] arr2 = {8, 9, 4, 5};
+        int[] expectedResult = {};
+
+        // act
+        int[] actualResult = new Intersection().intersection(arr1, arr2);
+
+        // assert
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testIntersection_SecondEmptyArray_Negative() {
+        // arrange
+        int[] arr1 = {8, 9, 4, 5};
+        int[] arr2 = {};
+        int[] expectedResult = {};
+
+        // act
+        int[] actualResult = new Intersection().intersection(arr1, arr2);
+
+        // assert
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testIntersection_SecondNullArray_Negative() {
+        // arrange
+        int[] arr1 = {8, 9, 4, 5};
+        int[] arr2 = null;
         int[] expectedResult = {};
 
         // act
